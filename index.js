505cc82202266f596d97ade02a9162e0e3ed4841
Ante Borzić <aborzic@extensionengine.com>
2021-03-03 09:53:16 +0100

    Add simple domain validation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const Arbiquelize = require('./lib/arbiquelize');
+const Errors = require('./lib/Errors');
 const Sqlize = require('./lib/sqlize');
 const Sync = require('./lib/sync');
 
 module.exports = {
   Arbiquelize,
+  Errors,
   Sqlize,
   Sync
 };
