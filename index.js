f62201fa0168621ff21d80d7e93cdfcc439e3536
Ante Borzić <aborzic@extensionengine.com>
2021-03-19 13:57:49 +0100

    Expose loader util

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const Arbiquelize = require('./lib/arbiquelize');
 const Errors = require('./lib/Errors');
 const Sqlize = require('./lib/sqlize');
 const Sync = require('./lib/sync');
+const Utils = require('./lib/utils');
 
 module.exports = {
   Arbiquelize,
   Errors,
   Sqlize,
-  Sync
+  Sync,
+  Utils
 };
